cd1d0b075d5b7822281e6a0a328b5f5c37134a10
Song Ha Jung <hajungs94@gmail.com>
2022-03-30 15:55:35 +0900

    configure head

--- game/Main.c
+++ game/Main.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+﻿#include "std.h"
 
 int main(void)
 {
---
+++ game/std.h
@@ -0,0 +1,18 @@
+#pragma once //
+
+//include -> 헤더파일에 있는 걸 포함한다라는 뜻
+
+
+
+
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h> //논리개수를 저장하는 헤더(데이터가 0이아닌 값이면 true)
